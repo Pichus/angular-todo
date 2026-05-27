a73c9e06e3a54ef894b7bf82d5d24611b077e661
pichus <dimatyzuk@gmail.com>
2026-05-27 13:59:00 +0300

    chore: remove calendar tab

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,10 @@
 import { Component } from '@angular/core';
-import { MatTabsModule } from '@angular/material/tabs';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { TaskCalendarComponent } from './components/task-calendar/task-calendar';
 import { TaskListComponent } from './components/task-list/task-list';
 
 @Component({
   selector: 'app-root',
-  imports: [MatToolbarModule, MatTabsModule, TaskListComponent, TaskCalendarComponent],
+  imports: [MatToolbarModule, TaskListComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss',
 })
