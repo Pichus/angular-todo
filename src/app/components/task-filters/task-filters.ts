d396e45c37f3d4357eb237fee5c5066f1be36bce
pichus <dimatyzuk@gmail.com>
2026-05-27 13:36:06 +0300

    feat: task filters - search and status toggle

---
+++ src/app/components/task-filters/task-filters.ts
@@ -0,0 +1,32 @@
+import { Component, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { Store } from '@ngrx/store';
+import { TASK_STATUSES, TaskStatus } from '../../store/task.model';
+import { TasksActions } from '../../store/tasks.actions';
+import { selectFilter } from '../../store/tasks.selectors';
+
+@Component({
+  selector: 'app-task-filters',
+  imports: [MatFormFieldModule, MatInputModule, MatButtonToggleModule, MatIconModule],
+  templateUrl: './task-filters.html',
+  styleUrl: './task-filters.scss',
+})
+export class TaskFiltersComponent {
+  private store = inject(Store);
+
+  filter = toSignal(this.store.select(selectFilter));
+  statuses: (TaskStatus | 'all')[] = ['all', ...TASK_STATUSES];
+
+  onSearchChange(event: Event): void {
+    const search = (event.target as HTMLInputElement).value;
+    this.store.dispatch(TasksActions.setFilter({ filter: { search } }));
+  }
+
+  onStatusChange(status: TaskStatus | 'all'): void {
+    this.store.dispatch(TasksActions.setFilter({ filter: { status } }));
+  }
+}
